86c9d42c573526b876eee124a18804d6afa4f89b
Thomas Holenstein <tholenst@google.com>
2025-08-22 05:23:43 -0700

    Add "Tink Script ID"s to all "run_tests.sh" scripts.
    PiperOrigin-RevId: 798171356
    Change-Id: Ia324f9ae7e0fc9110f17a82372adb7756592760e

--- kokoro/macos_external/bazel/run_tests.sh
+++ kokoro/macos_external/bazel/run_tests.sh
@@ -14,6 +14,11 @@
 # limitations under the License.
 ################################################################################
 
+# Generated with openssl rand -hex 10
+echo "==========================================================================="
+echo "Tink Script ID: 6906b784f5463d645563 (to quickly find the script from logs)"
+echo "==========================================================================="
+
 set -euo pipefail
 
 BAZEL_CMD="bazel"
